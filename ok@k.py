a5fb32b839c075d94aa3ceb0ae7813d4cf0ff31d
unknown <smirnov.dima.kazan@gmail.com>
2026-06-26 11:11:58 +0300

    new_prog

--- ok@k.py
+++ ok@k.py
@@ -1,1 +1,13 @@
-print("1342")
+def build_query_string(base_url, **kwargs):
+    result = ''
+    args_result = []
+    args_list = list(kwargs.items())
+    for arg in args_list:
+        #print(arg)
+        print(f"{arg[0]} = {arg[1]}")
+        args_result.append(f"{arg[0]} = {arg[1]}")
+    joined_args = "&".join(args_result)
+    print(joined_args)
+    return result
+
+print(build_query_string("https://api.weather.com/v1/forecast/", city="Moscow", days=3, unit="metric"))
